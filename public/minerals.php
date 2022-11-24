6c9bf93664856569428f7df982ed998490700ca4
Tatiana Barbosa <tpabarbosa@yahoo.com.br>
2022-11-24 16:18:57 -0300

    fix require dir

--- public/minerals.php
+++ public/minerals.php
@@ -2,8 +2,8 @@
 
 use Tpabarbosa\FindBestImageByColor\ImagesColorData;
 
-require_once '../vendor/autoload.php';
-require_once '../src/bootstrap.php';
+require_once __DIR__ . '../vendor/autoload.php';
+require_once __DIR__ . '../src/bootstrap.php';
 
 $toView = [
     'minerals' => $data,
